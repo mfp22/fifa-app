ee36ebcae33f6fee37db3f0bb7c264eb2effea11
Michael Pearson <mfpears@gmail.com>
2023-10-25 01:03:41 -0700

    Install StateAdapt

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,26 @@ import Header from 'components/header';
 import GlobalStyle from 'themes/global';
 import dark from 'themes/dark';
 import light from 'themes/light';
-import { useStore } from 'store';
-
+import { AdaptContext } from '@state-adapt/react';
+import { stateAdapt } from './state-adapt';
 
 const App = () => {
 
   const theme = useStore((state) => state.theme);
 
   return (
-    <ThemeProvider theme={theme === 'dark' ? dark : light}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <AdaptContext.Provider value={stateAdapt}>
+      <ThemeProvider theme={theme === 'dark' ? dark : light}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </AdaptContext.Provider>
   );
 };
 
